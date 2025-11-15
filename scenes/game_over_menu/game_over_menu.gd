2fb4933afc7542bd7ad36ceef65a2a688d40aa61
wuhsel <philippmeyer5@hotmail.de>
2025-11-15 19:56:34 +0100

    Fixed game over menu not registering with game manager

--- scenes/game_over_menu/game_over_menu.gd
+++ scenes/game_over_menu/game_over_menu.gd
@@ -4,6 +4,7 @@ extends Control
 
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
+	GameManagerSignalBus.game_over_menu_initiated.emit(self)
 
 func _on_quit_button_pressed():
 	GameManagerSignalBus.switch_scene_by_path.emit("res://scenes/main_menu/main_menu.tscn")
