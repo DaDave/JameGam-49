4cf1a5b594b746d1a1d88c277922a7f5b8fb32a0
wuhsel <philippmeyer5@hotmail.de>
2025-11-15 11:37:00 +0100

    Inserted button logic into checkpoint object

--- scenes/checkpoint_object/checkpoint_object.gd
+++ scenes/checkpoint_object/checkpoint_object.gd
@@ -2,7 +2,26 @@ class_name CheckpointObject
 
 extends StaticBody2D
 
+@export_subgroup("Nodes")
+@export var sprite: Sprite2D
+@export var collision_shape: CollisionShape2D
+
+@export var up_button_sprite: Texture2D
+@export var down_button_sprite: Texture2D
+@export var up_button_active_sprite: Texture2D
+
 @export var room_number: int
 
+var interactable = true
+var is_pressed = false
+
 func _ready() -> void:
-	visible = false
+	#visible = false
+	pass
+
+func interact():
+	if interactable:
+		is_pressed = true
+		sprite.texture = down_button_sprite
+		await get_tree().create_timer(1).timeout
+		sprite.texture = up_button_active_sprite
