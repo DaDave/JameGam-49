85a80b2fc96567ec6f1f439a2e147d78eb268df2
wuhsel <philippmeyer5@hotmail.de>
2025-11-15 12:57:12 +0100

    Small fix for iframes and added camera to player

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -6,7 +6,7 @@ class_name Player extends CharacterBody2D
 @export var input_component: InputComponent
 
 @export_subgroup("Settings")
-@export var iframe_length = 1 # in seconds
+@export var iframe_length = 0.2 # in seconds
 
 var is_iframed = false
 
@@ -15,6 +15,7 @@ func _process(delta: float) -> void:
 	animation_component.handle_dodge_roll_animation(input_component.wants_to_dodge(), input_component.input_horizontal, input_component.input_vertical)
 	movement_component.handle_movement(self, input_component.input_horizontal, input_component.input_vertical)
 	movement_component.handle_dodge(self, animation_component.is_dodging, input_component.input_horizontal, input_component.input_vertical)
+	_handle_dodge_iframe(animation_component.is_dodging)
 	move_and_slide()
 	
 	for i in get_slide_collision_count():
@@ -34,4 +35,5 @@ func _handle_dodge_iframe(is_dodging): #TODO: noch testen
 		is_iframed = true
 		await get_tree().create_timer(iframe_length).timeout
 		self.set_collision_layer_value(3, true)
+	if !is_dodging and is_iframed:
 		is_iframed = false
