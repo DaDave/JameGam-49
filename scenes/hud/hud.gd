d7221c81147780c91ba1a7d705eb113a36d49fae
wuhsel <philippmeyer5@hotmail.de>
2025-11-16 13:32:55 +0100

    Added countdown timer in HUD

--- scenes/hud/hud.gd
+++ scenes/hud/hud.gd
@@ -3,6 +3,10 @@ class_name Hud extends Control
 @export_subgroup("Nodes")
 @export var healthbar: HBoxContainer
 @export var heart_scene: PackedScene
+@export var countdown_label: Label
+
+@export_subgroup("Settings")
+@export var countdown_label_text: String = "{0} seconds til detonation!"
 
 var is_initialized = false
 var hearts:Array = []
@@ -10,6 +14,10 @@ var hearts:Array = []
 func _ready():
 	GameManagerSignalBus.game_started.connect(_on_game_started)
 	GameManagerSignalBus.decrease_player_health.connect(_on_decrease_player_health)
+	
+	
+func _process(delta) -> void:
+	countdown_label.text = countdown_label_text.format([roundi(GameTimer.time_left)])
 
 func add_heart():
 	var new_heart = heart_scene.instantiate()
@@ -21,6 +29,7 @@ func remove_heart():
 		var last_heart = hearts.pop_back()
 		healthbar.remove_child(last_heart)
 		last_heart.queue_free()
+		
 
 func _on_game_started():
 	var heart_count = GameManager.max_player_health
